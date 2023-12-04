4e798befaa9894e06ff278d420fd4173d6f22b2b
andjelandreski <andreskiandjela1@gmail.com>
2023-12-04 22:37:50 +0100

    Domaci broj 7

---
+++ D_7_p8bt63/script.js
@@ -0,0 +1,18 @@
+//Zadatak 1
+let v = 30;
+let n = 10;
+
+if(v/n >= 3){
+    document.write("<h1 style='color:green;'>DA</h1>");
+} else {
+    document.write("<h1 style='color:red;'>NE</h1>");
+}
+
+//Zadatak 2
+let stanovnici = 1000;
+let testirani = 700;
+let pozitivni = 500;
+
+if(pozitivni > stanovnici*0.1 && pozitivni > testirani*0.3){
+    document.write("<h1 style='color:red;'>VANREDNO STANJE</h1>");
+}
