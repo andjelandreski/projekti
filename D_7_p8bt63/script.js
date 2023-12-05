808217889fd56d9f0789d95a05fe4919a8feaab4
andjelandreski <andreskiandjela1@gmail.com>
2023-12-05 21:42:36 +0100

    ispravka domaceg broj 7

--- D_7_p8bt63/script.js
+++ D_7_p8bt63/script.js
@@ -1,11 +1,12 @@
 //Zadatak 1
 let v = 30;
-let n = 10;
+let n = 15;
 
 if(v/n >= 3){
     document.write("<h1 style='color:green;'>DA</h1>");
 } else {
     document.write("<h1 style='color:red;'>NE</h1>");
+    document.write('Potrebno je da napusti lokal: ' + (n-v/3));
 }
 
 //Zadatak 2
