efd64a6e75a5153db967b57af72c94e5b0d6d6c6
andjelandreski <andreskiandjela1@gmail.com>
2023-12-17 16:32:09 +0100

    promena u js

--- D_10_p8bt63/script.js
+++ D_10_p8bt63/script.js
@@ -1,27 +1,36 @@
 //Zadatak 1
 let spisak = ["Mleko", "Brašno", "Ulje", "Voda", "Piletina", "Kafa", "Hleb", "Banane", "Limun"];
-let lista = `<ul>`;
-for (i = 0; i < spisak.length; i++){
-    lista += `<li>${spisak[i]}</li>`;
-}
+let nabavka = niz => {
+    let lista = `<ul>`;
+    for (i = 0; i < niz.length; i++){
+    lista += `<li>${niz[i]}</li>`;
+    }
 lista += `</ul>`;
 document.body.innerHTML += lista;
+}
+nabavka(spisak);
+
 
 //Zadatak 2
 let KosKlubovi = ["Crvena zvezda", "Borac", "Dunav", "Dynamic", "Kolubara", "Metalac", "Partizan"];
-let tabela = `<table>`;
-for (i = 0; i < KosKlubovi.length; i++){
-    tabela += `<tr><td>${KosKlubovi[i]}</td></tr>`;
+let kosarka = niz => {
+    let tabela = `<table>`;
+    for (i = 0; i < niz.length; i++){
+        tabela += `<tr><td>${niz[i]}</td></tr>`;
+    }
+    tabela += `</table>`;
+    document.body.innerHTML += tabela;
 }
-tabela += `</table>`;
-document.body.innerHTML += tabela;
+kosarka(KosKlubovi);
 
 
 //Zadatak 3
 let nizSlika = ["1.jpg", "2.jpg", "3.jpg"];
-
-for(i = 0; i < nizSlika.length; i++){
-    let fotografija = `<img src="${nizSlika[i]}">`;
-    document.body.innerHTML += fotografija;    
+let fotografije = niz => {
+    for(i = 0; i < niz.length; i++){
+        let fotografija = `<img src="${niz[i]}">`;
+        document.body.innerHTML += fotografija; 
+    }
 }
+fotografije(nizSlika);
 
