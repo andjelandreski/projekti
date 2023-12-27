a9c40b102d1292440e84aeecc0db8c94094d43fa
andjelandreski <andreskiandjela1@gmail.com>
2023-12-27 22:39:04 +0100

    Domaci broj 13

---
+++ D_13_p8bt63/script.js
@@ -0,0 +1,48 @@
+let knjiga1 = {
+    title: "Trajan",
+    author: "Andjelko Krstic",
+    image: "slike/knjiga1.png",
+    procitano: true
+}
+
+let knjiga2 = {
+    title: "Secanja",
+    author: "Andjelko Krstic",
+    image: "slike/knjiga2.jpg",
+    procitano: false
+}
+
+let knjiga3 = {
+    title: "Pripovetke",
+    author: "Andjelko Krstic",
+    image: "slike/knjiga3.jpg",
+    procitano: true
+}
+let nizKnjiga = [knjiga1, knjiga2, knjiga3];
+
+let kol1 = document.querySelectorAll(".col1");
+let kol2 = document.querySelectorAll(".col2");
+let tab = document.getElementsByTagName("tr");
+let nizTab = Array.from(tab);
+
+for(let i = 0; i < kol1.length; i++){
+    kol1[i].innerHTML += `<img width="150px" src=${nizKnjiga[i].image}>`;
+
+}
+for(let i = 0; i < kol2.length; i++){
+    if(nizKnjiga[i].procitano == true){
+        kol2[i].innerHTML += `<p style="color:blue">${nizKnjiga[i].title}</p><p style="color:blue">${nizKnjiga[i].author}</p>`;
+    } else {
+        kol2[i].innerHTML += `<p style="color:grey">${nizKnjiga[i].title}</p><p style="color:grey">${nizKnjiga[i].author}</p>`;
+    }
+}
+
+let br = 0;
+nizTab.forEach(el => {
+    if(br % 2 == 0){
+        el.style.backgroundColor = "yellow";
+    } else {
+        el.style.backgroundColor = "beige";
+    }
+    br++;
+});
