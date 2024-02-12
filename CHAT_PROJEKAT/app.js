05186bf9fa2f40838f841e0b0614962836624617
andjelandreski <andreskiandjela1@gmail.com>
2024-02-12 15:57:47 +0100

    CHAT projekat

--- CHAT_PROJEKAT/app.js
+++ CHAT_PROJEKAT/app.js
@@ -11,6 +11,11 @@ let btnGeneral = document.getElementById("btnGeneral");
 let btnJs= document.getElementById("btnJs");
 let btnHomeworks = document.getElementById("btnHomeworks");
 let btnTests = document.getElementById("btnTests");
+let notification = document.getElementById("notification");
+let trenutniBtn = document.getElementById("btnGeneral");
+let inpColor =  document.getElementById("inpColor");
+let btnMenjanjeBoje = document.getElementById("btnMenjanjeBoje");
+let section = document.getElementById("prikazPoruka");
 
 //Lokalna memorija
 let username = "anonymus";
@@ -18,6 +23,9 @@ if(localStorage.username){
     username = localStorage.username;
 }
 
+let backgroundColor = localStorage.getItem("color");
+section.style.backgroundColor = backgroundColor;
+document.body.style.backgroundColor = backgroundColor;
 let sobaUkojojSeNalazim = "#general";
 //Objekti
 let chatroom = new Chatroom(sobaUkojojSeNalazim, username);
@@ -26,7 +34,7 @@ let chatui = new ChatUI(ul);
 
 //Prikaz poruka na stranici
 chatroom.getChats(data => {
-    chatui.list.innerHTML += chatui.templateLi(data);
+    chatui.list.innerHTML += chatui.templateLi(data, username);
     //templateLi(data);
 });
 
@@ -34,59 +42,108 @@ chatroom.getChats(data => {
 
 btnSlanjePor.addEventListener("click", (e)=> {
     e.preventDefault();
+    if(!unosPoruke.value.trim()){
+        alert("Unesite sadržaj poruke!");
+        return;
+    }
     chatroom.addChat(unosPoruke.value);
     unosPoruke.value = "";
 });
 
-btnKorisIme.addEventListener("click", (e)=> {
-    e.preventDefault();
+btnKorisIme.addEventListener("click", ()=> {
+    //e.preventDefault();
+    if(!inpKorisIme.value.trim()){
+        alert("Unesi ispravno korisničko ime!");
+        return;
+    }
+    if(inpKorisIme.value.length < 2 || inpKorisIme.value.length > 10){
+        alert("Unesi ispravno korisničko ime! Dužina korisničkog imena mora imati između 2 i 10 karaktera.");
+        return;
+    }
     let novoKorisIme = inpKorisIme.value;
     chatroom.username = novoKorisIme;
+    notification.innerHTML = `<p id="notificationPar">Korisnicko ime: ${novoKorisIme}</p>`;
+    setTimeout(function() {
+        notification.innerHTML = "";
+      }, 3000)
     localStorage.setItem("username", novoKorisIme);
     inpKorisIme.value = "";
+    location.reload();
 });
+ 
 
 btnGeneral.addEventListener("click", (e)=> {
     e.preventDefault();
     chatui.delete();
+    chatui.changeColor(btnGeneral, trenutniBtn);
+    trenutniBtn = btnGeneral;
     sobaUkojojSeNalazim = "#general";
     localStorage.setItem("room", sobaUkojojSeNalazim);
     chatroom.room = sobaUkojojSeNalazim;
     chatroom.getChats(data => {
-        chatui.list.innerHTML += chatui.templateLi(data);
+        chatui.list.innerHTML += chatui.templateLi(data, chatroom.username);
     });
-    
 });
 
 btnJs.addEventListener("click", (e)=> {
     e.preventDefault();
     chatui.delete();
+    chatui.changeColor(btnJs, trenutniBtn);
+    trenutniBtn = btnJs;
     sobaUkojojSeNalazim = "#js";
     localStorage.setItem("room", sobaUkojojSeNalazim);
     chatroom.room = sobaUkojojSeNalazim;
     chatroom.getChats(data => {
-        chatui.list.innerHTML += chatui.templateLi(data);
+        chatui.list.innerHTML += chatui.templateLi(data, chatroom.username);
     });
 });
 
 btnHomeworks.addEventListener("click", (e)=> {
     e.preventDefault();
     chatui.delete();
+    chatui.changeColor(btnHomeworks, trenutniBtn);
+    trenutniBtn = btnHomeworks;
     sobaUkojojSeNalazim = "#homeworks";
     localStorage.setItem("room", sobaUkojojSeNalazim);
     chatroom.room = sobaUkojojSeNalazim;
     chatroom.getChats(data => {
-        chatui.list.innerHTML += chatui.templateLi(data);
+        chatui.list.innerHTML += chatui.templateLi(data, chatroom.username);
     });
 });
 
 btnTests.addEventListener("click", (e)=> {
     e.preventDefault();
     chatui.delete();
+    chatui.changeColor(btnTests, trenutniBtn);
+    trenutniBtn = btnTests;
     sobaUkojojSeNalazim = "#tests";
     localStorage.setItem("room", sobaUkojojSeNalazim);
     chatroom.room = sobaUkojojSeNalazim;
     chatroom.getChats(data => {
-        chatui.list.innerHTML += chatui.templateLi(data);
+        chatui.list.innerHTML += chatui.templateLi(data, chatroom.username);
     });
 });
+
+
+btnMenjanjeBoje.addEventListener("click", (e)=>{
+    e.preventDefault();
+    backgroundColor = inpColor.value
+    section.style.backgroundColor = backgroundColor;
+    document.body.style.backgroundColor = backgroundColor;
+
+    localStorage.setItem("color", backgroundColor);
+});
+
+/*console.log(ul);
+console.log(ul.lastChild);
+let li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li.lastElementChild);
+/*kanta.addEventListener("click", (e)=> {
+    console.log(`kliknuo si`);
+    /*if (confirm('Da li ste sigurni da želite da obrišete ovu poruku?')) {
+        console.log(`stisnuto je ok`);
+        } else {
+        console.log(`nije ok`);
+    }
+});*/
--- CHAT_PROJEKAT/ui.js
+++ CHAT_PROJEKAT/ui.js
@@ -12,15 +12,31 @@ export class ChatUI {
     }
 
     formatDate(vreme) {
-        return `${vreme.getDate()}.${vreme.getMonth()+1}.${vreme.getFullYear()}. - ${vreme.getHours()}:${vreme.getMinutes()}`;
+        if(vreme.getDate() == new Date().getDate()){
+            return `${vreme.getHours().toString().padStart(2, '0')}:${vreme.getMinutes().toString().padStart(2, '0')}`;
+        } else {
+            return `${vreme.getDate().toString().padStart(2, '0')}.${(vreme.getMonth()+1).toString().padStart(2, '0')}.${vreme.getFullYear().toString().padStart(2, '0')}. - ${vreme.getHours().toString().padStart(2, '0')}:${vreme.getMinutes().toString().padStart(2, '0')}`;
+        }
+        
     }
-    
-    templateLi(data){
-        return `<li>${data.username}: ${data.message}<br>${this.formatDate(data.created_at.toDate())}</li>`;
+
+    templateLi(data, trenutni){
+        if (data.username === trenutni) {
+            return `<li class="liRight">${data.username}: ${data.message}<br>${this.formatDate(data.created_at.toDate())}
+            <br><i class="fa fa-trash"></i></li>`;
+        }
+        return `<li>${data.username}: ${data.message}<br>${this.formatDate(data.created_at.toDate())}<br><i class="fa fa-trash"></i></li>`;
     }
 
     delete() {
         this.list.innerHTML = "";
     }
 
+    changeColor(btn, trenutni){
+        btn.classList.remove('buttons');
+        btn.classList.add('color-plum');
+        
+        trenutni.classList.remove('color-plum');
+        trenutni.classList.add('buttons');
+    }
 }
